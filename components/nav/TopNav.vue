361ed420c6c1163ecde344a1c558063bb8cfce32
Jakub Gramburg <jacubgr@gmail.com>
2023-03-31 20:08:37 +0200

    init + fix TopNav

---
+++ components/nav/TopNav.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="nav">
+        <img class="logo logo-contain" src="@/static/icon.png">
+        <div class="nav-links">
+            <span>
+                <NuxtLink class="item" to="/">Strona domowa</NuxtLink>
+            </span>
+            <span>
+                <NuxtLink class="item" to="/onas">O nas</NuxtLink>
+            </span>
+            <span>
+                <NuxtLink class="item" to="/onas">Galeria</NuxtLink>
+            </span>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+<style scoped>
+.nav {
+    display: flex;
+    /* flex-direction: column; */
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    border-radius: 10px;
+    background-color: #757575;
+    border-width: 10px;
+    max-height: 100px;
+    min-height: 100px;
+    max-width: 100vw;
+    min-width: 200px;
+}
+
+.logo {
+    width: 100px;
+    height: 100px;
+    margin-left: 2vw;
+}
+
+.logo-contain {
+    object-fit: contain;
+    object-position: center;
+}
+
+.nav-links {
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+}
+
+/* px, rem, em, vh, vw, % */
+.item {
+    margin-inline: 3px;
+    padding: 15px 32px;
+    border-radius: 4px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: large;
+    text-transform: uppercase;
+    background-color: #e7e7e7;
+    /* Green */
+    border: none;
+    color: black;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
+
+.item:hover {
+    background-color: #00E676;
+    /* Green */
+    color: white;
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <body>
+    <TopNav />
+  </body>
+</template>
+
+
+<script>
+import TopNav from '../components/nav/TopNav.vue'
+import bigImage from '../components/gallery/bigImage.vue'
+
+export default {
+
+  name: 'IndexPage',
+  components: {
+    TopNav,
+    bigImage
+  }
+}
+
+</script>
